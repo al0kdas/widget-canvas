e2448356b515661082e6330aa393a329d838f2b4
Alok Das <mail2alokdas@gmail.com>
2025-02-24 18:53:27 +0530

    removed timer

--- src/components/TableWidget.js
+++ src/components/TableWidget.js
@@ -32,15 +32,7 @@ const TableWidget = ({ content, onChange }) => {
 
     window.addEventListener('keydown', handleKeyDown);
     
-    // Set initial prompt timer
-    const promptTimer = setTimeout(() => {
-      tableCommandPrompt();
-    }, 500);
-    
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      clearTimeout(promptTimer);
-    };
+   
   }, [content]);
 
   const tableCommandPrompt = () => {
